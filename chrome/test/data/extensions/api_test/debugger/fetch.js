99a2d70103e7de3141bb9b7f5081980887c7fd1e
Joey Arhar <jarhar@chromium.org>
2019-01-07 14:14:54 -0800

    Reland^2 "[DevTools] Prevent JSONReader::Read errors from invalid unicode points"
    Bug: 824174
    Change-Id: I38db9c5355008ca0f31be525ec1c2c08f86d88b9

---
+++ chrome/test/data/extensions/api_test/debugger/fetch.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  fetch('/invalid_char.html')
+      .catch(err => {
+        console.err(`fetch('/invalid_char.html') failed with error: ${err}`);
+      });
+});
